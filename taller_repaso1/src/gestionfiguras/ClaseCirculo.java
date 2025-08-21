fdffb45f0abc2093fb2b6a911c130837eedcad78
AlejoRG21 <cesar.rivera.g@uniautonoma.edu.co>
2025-08-21 01:21:15 -0500

    Update ClaseCirculo.java

--- taller_repaso1/src/gestionfiguras/ClaseCirculo.java
+++ taller_repaso1/src/gestionfiguras/ClaseCirculo.java
@@ -1,6 +1,6 @@
 package gestionfiguras;
 
-public class ClaseCirculo extends ClaseFigura {
+public class ClaseCirculo extends ClaseFigura implements interface_figura{
     private float radio;
 
     public ClaseCirculo(float radio, String color) {
@@ -10,5 +10,25 @@ public class ClaseCirculo extends ClaseFigura {
 
     public float area() {
         return (float) Math.PI * radio * radio;
+    }
+
+    @Override
+    public void autor() {
+        System.out.println("Autor: Alejandro Rivera");
+    }
+
+    @Override
+    public int valor() {
+        return (int)radio;
+    }
+
+    @Override
+    public String descripcion() {
+        return "Círculo con radio " + radio;
+    }
+
+    @Override
+    public int numLados() {
+        return 0;
     }
 }
